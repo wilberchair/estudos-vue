f7464f3e6e7b66c61f1c595341bde8273e57785f
unknown <wilberchair@gmail.com>
2021-10-11 12:08:18 -0300

    finalizando desafio2

--- dom/desafio-02/app.js
+++ dom/desafio-02/app.js
@@ -1,6 +1,14 @@
 new Vue({
     el: '#desafio',
     data: {
-        valor: ''
+        valor: 'Digite aqui...'
+    },
+    methods: {
+        alerta() {
+            return alert('Alerta')
+        },
+        escutarEvento(event) {
+            return this.valor = event.target.value;
+        }
     }
 })
